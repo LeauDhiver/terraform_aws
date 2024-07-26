9233b59df8200a23a806bb4535546e7e15a78d53
LeauDhiver <leaudhiver94@gmail.com>
2024-07-26 17:01:38 +0900

    feat : Create s3 bucket & dynamodb table for backend.tf

---
+++ terraform/backend_init/init.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-northeast-2"
+  profile = "yeardream4"
+}
+
+resource "aws_s3_bucket" "tfstate"{
+  bucket = "tf101-eagle-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = "terraform-lock"
+  hash_key = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
