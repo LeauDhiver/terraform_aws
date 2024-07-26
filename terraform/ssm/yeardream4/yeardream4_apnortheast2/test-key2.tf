bee32f424e4093b538f3bb3955d41a4866a80665
LeauDhiver <leaudhiver94@gmail.com>
2024-07-26 14:21:08 +0900

    feat : Create test-key2.tf

---
+++ terraform/ssm/yeardream4/yeardream4_apnortheast2/test-key2.tf
@@ -0,0 +1,10 @@
+module "test-key2" {
+
+  source = "../_modules/aws_ssm_parameter"
+  ssm_parameter_name = "test-key2"
+  ssm_parameter_description = "for testing"
+  ssm_parameter_key_id = "alias/deployment"
+  ssm_parameter_value = var.secret-value2
+
+
+}
